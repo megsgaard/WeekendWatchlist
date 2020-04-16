8b723a581984bab1c35af3195b7b475e3f77c43f
FridaHJuul <frida.hvelplund.juul@post.au.dk>
2020-04-16 16:05:02 +0200

    Tilføjet implementation i build.gradle. Tilføjet kode i ListActivity som skal bruges til at snakke med fireStore (skal nok ligge et andet sted, når det kommer til stykket, men bare lige så vi har det indtil videre)

---
+++ app/src/main/java/au585303/au590400/weekendwatchlist/ListActivity.java
@@ -0,0 +1,81 @@
+package au585303.au590400.weekendwatchlist;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListActivity extends AppCompatActivity {
+
+    private FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
+    private ListenerRegistration itemsListener;
+    private String LOG = "ListActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //Kode direkte kopieret fra hans demo
+        Map<String,Object> item = new HashMap<>();
+        item.put("text",new Date().toString());
+
+        Task<DocumentReference> items = fireStore.collection("Items").add(item).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(LOG, "Added " + documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(LOG, e.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        itemsListener = fireStore.collection("Items").addSnapshotListener(
+                new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if(queryDocumentSnapshots != null  && !queryDocumentSnapshots.getDocuments().isEmpty())
+                        {
+                            List<String> items = new ArrayList<>();
+                            for(DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments())
+                            {
+                                items.add(snapshot.getData().get("text").toString());
+                            }
+                            //adapter.setItems(items);
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        itemsListener.remove();
+    }
+}
