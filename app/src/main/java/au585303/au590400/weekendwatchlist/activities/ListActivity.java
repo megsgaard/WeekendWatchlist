0f2f6e9b4e0f696d68f02543fbe70955a997968e
megsgaard <mathias.egsgaard@post.au.dk>
2020-04-28 22:37:38 +0200

    Bug fix: Update recycler view when movie is deleted.

--- app/src/main/java/au585303/au590400/weekendwatchlist/activities/ListActivity.java
+++ app/src/main/java/au585303/au590400/weekendwatchlist/activities/ListActivity.java
@@ -335,6 +335,7 @@ public class ListActivity extends AppCompatActivity implements ListAdapter.OnIte
                     public void onClick(DialogInterface dialog, int which) {
                         backgroundService.deleteMovie(movie.getTitle());
                         movies.remove(movie);
+                        adapter.notifyDataSetChanged();
                     }
                 }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
